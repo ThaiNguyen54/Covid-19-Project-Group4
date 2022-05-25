b6592505a2548427412dd0cf9b3cb4c278d21544
Thai Nguyen <19522188@gm.uit.edu.vn>
2022-05-25 23:38:23 +0700

    update code

--- Covid_19_WebApp_Project/Covid_19_WebApp_Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Covid_19_WebApp_Project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
