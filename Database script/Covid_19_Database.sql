5d7e67d9bf6669b88883d318981f45ddbce2ca3d
Thai Nguyen <19522188@gm.uit.edu.vn>
2022-06-06 23:15:02 +0700

    Update code

--- Database script/Covid_19_Database.sql
+++ Database script/Covid_19_Database.sql
@@ -12,32 +12,32 @@ Create table CITIZEN
 	LName varchar(100),
 	BDate date,
 	Address varchar(100),
-	Age int,
 	Password varchar(100),
 	Phone varchar(100),
-	Email varchar(100)
+	Email varchar(100),
+	Gender varchar(20)
 )
 
-Create table Vaccin_Registration_Form
+Create table VACCINE_REGISTRATION_FORM
 (
 	FormID int not null primary key,
 	CitizenID int not null,
-	VaccinID int not null,
+	VaccineID int not null,
 	InjectionDate date not null,
 	RegisterDate date not null,
 )
 
-Create table VACCIN_RECORD
+Create table VACCINE_RECORD
 (
 	RecordID int not null primary key,
 	CitizenID int not null,
 	Number_Injection int not null,
 	FirstDate date,
-	FirstVaccin varchar(100),
+	FirstVaccine varchar(100),
 	SecondDate date,
-	SecondVaccin varchar(100),
+	SecondVaccine varchar(100),
 	ThirdDate date,
-	ThirdVaccin varchar(100),
+	ThirdVaccine varchar(100),
 	First_NurseID int,
 	Second_NurseID int,
 	Third_NurseID int
@@ -51,7 +51,6 @@ Create table Nurse
 	LName varchar(100),
 	BDate date,
 	Address varchar(100),
-	Age int,
 	Phone varchar(100),
 	Password varchar(100),
 	Email varchar(100)
@@ -65,13 +64,13 @@ create table SUPPLIER
 	Phone varchar(100)
 )
 
-Create table VACCIN_ENTRY
+Create table VACCINE_ENTRY
 (
 	EntryID int not null primary key,
 	ManagerID int not null,
 	SupID int not null,
 	EntryDate date not null,
-	Amount_Vaccin int not null
+	Amount_Vaccine int not null
 )
 
 Create table MANAGER
@@ -81,30 +80,29 @@ Create table MANAGER
 	LName varchar(100),
 	BDate date,
 	Address varchar(100),
-	Age int,
-	Phone varchar(100)
+	Phone varchar(100),
 	Password varchar(100),
 	Email varchar(100)
 )
 
-Create table VACCIN
+Create table VACCINE
 (
-	VaccinID int not null primary key,
+	VaccineID int not null primary key,
 	WarehouseID int not null,
-	VaccinName varchar(100) not null
+	VaccineName varchar(100) not null
 )
 
 Create table ENTRY_DETAIL
 (
-	VaccinID int not null,
+	VaccineID int not null,
 	EntryID int not null,
 	Quantity int not null,
 	Primary key (VaccinID, EntryID)
 )
 
-Create table WAREHOUSE_VACCIN
+Create table WAREHOUSE_VACCINE
 (
-	VaccinID int not null,
+	VaccineID int not null,
 	WarehouseID int not null,
 	Stock int not null, 
 	Primary key (VaccinID, WarehouseID)
@@ -117,27 +115,50 @@ Create table WAREHOUSE
 )
 
 --Add foreign key for register form
-alter table [dbo].[Vaccin_Registration_Form] add constraint Register_Citizen foreign key (CitizenID) references [dbo].[CITIZEN](CitizenID)
-alter table [dbo].[Vaccin_Registration_Form] add constraint Register_Vaccin foreign key (VaccinID) references [dbo].[VACCIN](VaccinID)
+alter table [dbo].[VACCINE_REGISTRATION_FORM] add constraint Register_Citizen foreign key (CitizenID) references [dbo].[CITIZEN](CitizenID)
+alter table [dbo].[VACCINE_REGISTRATION_FORM] add constraint Register_Vaccin foreign key (VaccineID) references [dbo].[VACCINE](VaccineID)
 
 
 --Add foreign key for record 
-alter table [dbo].[VACCIN_RECORD] add constraint Record_Nurse foreign key (First_NurseID) references [dbo].[Nurse](NurseID)
-alter table [dbo].[VACCIN_RECORD] add constraint Record_SeconNurse foreign key (Second_NurseID) references [dbo].[Nurse](NurseID)
-alter table [dbo].[VACCIN_RECORD] add constraint Record_ThirdNurse foreign key (Third_NurseID) references [dbo].[Nurse](NurseID)
-alter table [dbo].[VACCIN_RECORD] add constraint Record_Citizen foreign key (CitizenID) references [dbo].[CITIZEN](citizenID)
+alter table [dbo].[VACCINE_RECORD] add constraint Record_Nurse foreign key (First_NurseID) references [dbo].[Nurse](NurseID)
+alter table [dbo].[VACCINE_RECORD] add constraint Record_SeconNurse foreign key (Second_NurseID) references [dbo].[Nurse](NurseID)
+alter table [dbo].[VACCINE_RECORD] add constraint Record_ThirdNurse foreign key (Third_NurseID) references [dbo].[Nurse](NurseID)
+alter table [dbo].[VACCINE_RECORD] add constraint Record_Citizen foreign key (CitizenID) references [dbo].[CITIZEN](citizenID)
 
 --Add foreign key for Nurse
 alter table [dbo].[Nurse] add constraint Nurse_Manager foreign key (ManagerID) references [dbo].[MANAGER](ManagerID)
 
 --Add foreign key for Vaccin Entry
-alter table [dbo].[VACCIN_ENTRY] add constraint Entry_Supplier foreign key (SupID) references [dbo].[SUPPLIER](SupID)
-alter table [dbo].[VACCIN_ENTRY] add constraint Entry_Manager foreign key (managerID) references [dbo].[MANAGER](ManagerID)
+alter table [dbo].[VACCINE_ENTRY] add constraint Entry_Supplier foreign key (SupID) references [dbo].[SUPPLIER](SupID)
+alter table [dbo].[VACCINE_ENTRY] add constraint Entry_Manager foreign key (managerID) references [dbo].[MANAGER](ManagerID)
 
 --Add foreign key for Entry Detail
-alter table [dbo].[ENTRY_DETAIL] add constraint Detail_Entry foreign key (EntryID) references [dbo].[VACCIN_ENTRY](EntryID)
-alter table [dbo].[ENTRY_DETAIL] add constraint Detail_Vaccin foreign key (VaccinID) references [dbo].[VACCIN](VaccinID)
+alter table [dbo].[ENTRY_DETAIL] add constraint Detail_Entry foreign key (EntryID) references [dbo].[VACCINE_ENTRY](EntryID)
+alter table [dbo].[ENTRY_DETAIL] add constraint Detail_Vaccin foreign key (VaccineID) references [dbo].[VACCINE](VaccineID)
 
 --Add foreing key for Warehouse_Vaccin
-alter table [dbo].[WAREHOUSE_VACCIN] add constraint Warehouse_And_Vaccin foreign key (VaccinID) references [dbo].[VACCIN](VaccinID)
-alter table [dbo].[WAREHOUSE_VACCIN] add constraint Warehouse_And_warehouse foreign key (WarehouseID) references [dbo].[WAREHOUSE](WarehouseID)
+alter table [dbo].[WAREHOUSE_VACCINE] add constraint Warehouse_And_Vaccine foreign key (VaccineID) references [dbo].[VACCINE](VaccinID)
+alter table [dbo].[WAREHOUSE_VACCINE] add constraint Warehouse_And_warehouse foreign key (WarehouseID) references [dbo].[WAREHOUSE](WarehouseID)
+
+
+
+-- Insert Vaccine Record Trigger
+
+create or alter trigger insert_register_form
+on VACCINE_REGISTRATION_FORM
+after insert
+as 
+	declare @cid int, @date_ineject date, @first_inject_date date
+begin
+	select @cid = CitizenID, @date_ineject = InjectionDate
+	from inserted
+
+	select @first_inject_date = FirstDate
+	from VACCINE_RECORD
+
+	if(datediff(day,@first_inject_date, @date_ineject) < 60)
+	begin
+		print'not enough days'
+		rollback transaction
+	end
+end
---
+++ Database script/Insert data WAREHOUSE.sql
@@ -0,0 +1,4 @@
+insert into WAREHOUSE (warehouseid, Location) values (1, '87 Mayer Terrace');
+insert into WAREHOUSE (warehouseid, Location) values (2, '86 3rd Street');
+insert into WAREHOUSE (warehouseid, Location) values (3, '10317 Maywood Place');
+insert into WAREHOUSE (warehouseid, Location) values (4, '8 Rowland Parkway');
