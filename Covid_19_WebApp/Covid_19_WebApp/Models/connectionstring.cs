60ec8233e3a9c4ba195258cdfd56f13a0e89feca
Thai Nguyen <19522188@gm.uit.edu.vn>
2022-06-16 23:56:47 +0700

    Update Back-End and Database Script

--- Covid_19_WebApp/Covid_19_WebApp/Models/connectionstring.cs
+++ Covid_19_WebApp/Covid_19_WebApp/Models/connectionstring.cs
@@ -2,14 +2,12 @@
 {
     public class connectionstring
     {
-        private string SqlServer = @"Data Source=DESKTOP-S89DF0S;Initial Catalog=Covid_19;Integrated Security=True";
-        private string AzureconnectionString = "Server=tcp:webprojectcovid19.database.windows.net,1433;Initial Catalog=Covid_19_Dose;Persist Security Info=False;User ID=thainguyen;Password=Thai******:::542001;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        
+        private string SqlServer = @""
 
 
         public string GetConnectionString()
         {
-            //return this.AzureconnectionString;
+
             return this.SqlServer;
         }
     }
